d99200f7d0741401fcd69db32fe0267f552cde68
Piotr Esden-Tempski <piotr@esden.net>
2015-12-28 17:13:32 +0100

    Added PWM example.

---
+++ pwm/pwm.v
@@ -0,0 +1,19 @@
+module top(
+	input clk,
+	output LED1,
+	output LED2,
+	output LED3,
+	output LED4,
+	output LED5,
+);
+
+	reg [23:0] cnt;
+
+	always @(posedge clk) cnt<=cnt+1;
+	wire [3:0] PWM_input = cnt[23] ? cnt[22:19] : ~cnt[22:19];    // ramp the PWM input up and down
+
+	reg [4:0] PWM;
+	always @(posedge clk) PWM <= PWM[3:0]+PWM_input;
+
+	assign LED1 = PWM[4];
+endmodule

e9777461b6c2eff9a7b6d260971d71d171f0400f
Piotr Esden-Tempski <piotr@esden.net>
2016-03-25 01:01:05 -0700

    Use an array for leds instead of individial pins.

--- hx8kboard/counter/counter.v
+++ hx8kboard/counter/counter.v
@@ -1,13 +1,6 @@
 module top (
 	input  clk,
-	output LED0,
-	output LED1,
-	output LED2,
-	output LED3,
-	output LED4,
-	output LED5,
-	output LED6,
-	output LED7
+	output [7:0] LED
 );
 
 	localparam BITS = 8;
@@ -21,5 +14,5 @@ module top (
 		outcnt <= counter >> LOG2DELAY;
 	end
 
-	assign {LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7} = outcnt;
+	assign LED = outcnt;
 endmodule
